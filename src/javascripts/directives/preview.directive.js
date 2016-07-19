172c7e414004e81e32e1f1aad986a64a1555bfc5
Masoud Mirzaei <mamos98@gmail.com>
2016-07-19 21:51:07 +0430

    Fix Range Slider in Edge and make sidebar Sticky on Repeat End

--- src/javascripts/directives/preview.directive.js
+++ src/javascripts/directives/preview.directive.js
@@ -11,8 +11,18 @@
       restrict : 'E',
       templateUrl: '/static/templates/preview.directive.html',
       controller: 'PreviewController',
-      controllerAs: 'vm'
-    }
+      controllerAs: 'vm',
+      link: function (scope,elem,attrs) {
+        // Determine if last ng-repeat item has been loaded and then
+        // Bind foundation to document
+        if (scope.$last === true) {
+            elem.ready(function () {
+                $(document).foundation();
+                console.log("Sticky Started");
+            });
+        }
+      }
+    };
   }
 
 })();
